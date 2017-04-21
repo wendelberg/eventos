e997dfca8e79fccef793cab22a6ecf1a5e74957a
wendelberg <wendelberg@wn0040.wine.com.br>
2017-04-21 04:32:36 -0300

    Atualização data de pagamento

--- gestao-projeto/grails-app/controllers/br/com/controleAcesso/UsuarioController.groovy
+++ gestao-projeto/grails-app/controllers/br/com/controleAcesso/UsuarioController.groovy
@@ -262,7 +262,7 @@ class UsuarioController {
 			calendar.add(Calendar.MONTH, 1)
 		}
 		
-		calendar.set(Calendar.DAY_OF_MONTH, 10)
+		calendar.set(Calendar.DAY_OF_MONTH, 5)
 		 
 		int parcelas = Integer.parseInt(params.tipoInscricaoValor.maxParcelas) 
 		println parcelas
